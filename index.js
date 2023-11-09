6613aa2e6137978e1ec0e7f245c5ac270be39bd0
Maddie Weber <maddieweber7@gmail.com>
2023-11-08 21:59:23 -0500

    done

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  let splitword = word.split('');
+
+  // Make a copy of the array before reversing it
+  let revArray = [...splitword].reverse();
+
+  let revWord = revArray.join('');
+
+  if (word === revWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+const testWord = "madam";
+console.log(isPalindrome(testWord)); 
+// Output: true
+
+isPalindrome('abba');
+isPalindrome('racecar');
+
+
 /* 
-  Add your pseudocode here
+Find length of characters of the string 
+divide length of characters by two and round up
+return the first half in an array and rearrange the array so that it is backwards
+create an array of the last half of the string and compare to the first
+
 */
 
 /*
   Add written explanation of your solution here
 */
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
+// module.exports = isPalindrome;
